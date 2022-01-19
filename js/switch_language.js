e9d3119dcf0b412363de2ceab1739450f8021507
Nilender-Andrey <nilenderav@mail.ru>
2022-01-19 22:02:19 +0400

    feat: add language change

--- js/switch_language.js
+++ js/switch_language.js
@@ -1,3 +1,21 @@
 import i18Obj from './translate.js';
 
-console.log(i18Obj);
+const headerLangsSwitcher = document.querySelector('.header__langs-switcher');
+
+headerLangsSwitcher.addEventListener('change', change);
+
+function change() {
+  const switchButtons = document.querySelectorAll('input[name=langs]');
+
+  switchButtons.forEach(
+    (item) => item.checked === true && switchLanguage(item.value),
+  );
+}
+
+function switchLanguage(language = 'en') {
+  const elementsI18 = document.querySelectorAll('[data-i18]');
+
+  elementsI18.forEach(
+    (item) => (item.textContent = i18Obj[language][item.dataset.i18]),
+  );
+}
