436e6c48694321c36bd8acbac6619dd4fe1cc554
Nilender-Andrey <nilenderav@mail.ru>
2022-01-22 10:41:07 +0400

    feat: add complex button effects

--- js/btn_animate.js
+++ js/btn_animate.js
@@ -1,17 +1,52 @@
-const animateButton = function (e) {
-  console.log('111');
-  e.preventDefault;
-  //reset animation
-  e.target.classList.remove('animate');
+const buttons = document.querySelectorAll('.button');
 
-  e.target.classList.add('animate');
-  setTimeout(function () {
-    e.target.classList.remove('animate');
-  }, 700);
-};
+buttons.forEach((item) => item.addEventListener('click', addElement));
 
-const buttonVar1 = document.querySelectorAll('.button-var1');
+function addElement(e) {
+  const addDiv = document.createElement('div');
+  const mValue = Math.max(this.clientWidth, this.clientHeight);
+  const position = this.getBoundingClientRect();
 
-buttonVar1.forEach((element) => {
-  element.addEventListener('click', animateButton, false);
-});
+  addDiv.style.width = addDiv.style.height = mValue + 'px';
+  addDiv.style.left = e.clientX - position.left - mValue / 2 + 'px';
+  addDiv.style.top = e.clientY - position.top - mValue / 2 + 'px';
+
+  addDiv.classList.add('pulse');
+  this.appendChild(addDiv);
+
+  setTimeout(() => addDiv.remove(), 1000);
+}
+
+document.addEventListener('mouseover', mouseover);
+document.addEventListener('mouseout', mouseout);
+
+function mouseover(e) {
+  if (e.target.classList.contains('button')) {
+    const btn = e.target;
+    const addDiv = document.createElement('div');
+    addDiv.classList.add('illumination');
+
+    const mValue = Math.max(btn.clientWidth, btn.clientHeight);
+    addDiv.style.width = addDiv.style.height = mValue + 'px';
+
+    btn.appendChild(addDiv);
+
+    btn.addEventListener('mousemove', mousemove);
+  }
+}
+
+function mouseout(e) {
+  if (e.target.classList.contains('button')) {
+    const btn = e.target;
+    btn.removeEventListener('mousemove', mousemove);
+    document.querySelectorAll('.illumination').forEach((i) => i.remove());
+  }
+}
+
+function mousemove(e) {
+  const position = e.target.getBoundingClientRect();
+  const illumination = document.querySelector('.illumination');
+  const mValue = Math.max(e.target.clientWidth, e.target.clientHeight);
+  illumination.style.left = e.clientX - position.left - mValue / 2 + 'px';
+  illumination.style.top = e.clientY - position.top - mValue / 2 + 'px';
+}
